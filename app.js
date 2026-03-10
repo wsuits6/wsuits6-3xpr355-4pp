868956d330c77c02a7b4f29c81a372b939093347
wsuits6 <wsuits6@gmail.com>
2026-03-10 22:38:42 +0000

    updated app.js

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ const app = express()
 const port = 3000
 
 // App logic to handle user HTTP  request
-app.get('/', (reg , res) => {
+app.get('/', (req , res) => {
     res.send('Hello World I am Wsuits6')
+})
+
+app.get('/login', (req, res) => {
+    res.send('Login Page Auth requird..... ')
+})
+
+app.get('/register', (req, res) => {
+    res.send("Register and join the legon of Express Devs hahahaha ")
 })
 
 // logic to retun to console if port is connected to  
 app.listen(port, ()=> {
+    console.log("my teacher will say why I used === in the code he thinks its AI ")
     console.log('<<<<<<========= Example app listing ==========>>>>>')
 })
