5ead316d397d1bb52902b785874d5bfcb90108dc
wsuits6 <wsuits6@gmail.com>
2026-03-10 22:21:39 +0000

    yhhh let goo

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+// create Express Object
 const express = require( 'express')
+
+//Assing Object to app variable
 const app = express()
+
+//Connection port to listen on
 const port = 3000
 
+// App logic to handle user HTTP  request
 app.get('/', (reg , res) => {
     res.send('Hello World I am Wsuits6')
 })
 
+// logic to retun to console if port is connected to  
 app.listen(port, ()=> {
     console.log('<<<<<<========= Example app listing ==========>>>>>')
 })
